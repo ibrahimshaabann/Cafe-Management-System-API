ca17549b51bdaf4f121fdcd8b52e725479c5b5bd
Ibrahim Shaaban <ibrahemhema2017@gmail.com>
2023-09-16 20:25:38 +0300

    search filters in hr_views

--- CafeAPI/human_resources/permissions.py
+++ CafeAPI/human_resources/permissions.py
@@ -40,9 +40,6 @@ class SalOrEmpAccessPermission(permissions.BasePermission):
     
     """
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        
         if request.method in ('POST', 'GET', 'PATCH', 'PUT', 'DELETE'):
             return request.user.role == User.Roles.ADMIN
         
